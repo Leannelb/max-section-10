98faf480e97fc9c84c03f0538e8c776ef94dbb41
leannelb <leannelaceybyrne@outlook.co>
2020-02-07 21:06:35 +0000

    slice() toi copy array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,14 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    recipes: Recipe[] = [
+    private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
       ];
+
+    getRecipes() {
+        return this.recipes.slice();
+        // this stops the actual array being returned and instead returns a copy
+    }
 }
+
