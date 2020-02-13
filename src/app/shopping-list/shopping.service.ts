e00fb8d1d2bff7913ca8fde35282ccb4cedc65de
leannelb <leannelaceybyrne@outlook.co>
2020-02-13 21:36:13 +0000

    app.component file moved. fixed that. ingredient name not showing, amout showing twice.

--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -1,14 +1,16 @@
+import { EventEmitter } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "protractor";
+
 
 export class ShoppingService {
 
     // we create an event for when an ingredient is changed, emitting an array of ingredients
     ingredientChanged = new EventEmitter<Ingredient[]>();
 
-   private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10),
+    private ingredients: Ingredient[] = [
+            new Ingredient('Apples', 5),
+            new Ingredient('Tomatoes', 10),
     ];
 
     // returns a copy of the private array.
@@ -19,7 +21,7 @@ export class ShoppingService {
     // before in the last commit, we didnt see the ingredient being added when
     // a new one was added, therefore we emit back the new array
     public addIngredient(ingredient: Ingredient) {
-        return this.ingredients.push(ingredient);
+        this.ingredients.push(ingredient);
         this.ingredientChanged.emit(this.ingredients.slice());
     }
 }
